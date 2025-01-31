9eef520d6af47c8f8caa823cf82eee57fc0fd51f
AghaulorGift-HNG12 <aghaulorgift74@gmail.com>
2025-01-31 19:14:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json({
-    email: "aghaulor.gift@gmail.com",
-    current_datetime: new Date().toISOString(),
-    github_url: "https://github.com/AghaulorGift-HNG12/backend",
-  });
+    return res.json({
+        email: "aghaulor.gift@gmail.com",
+        current_datetime: new Date().toISOString(),
+        github_url: "https://github.com/AghaulorGift-HNG12/backend"
+    });
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
